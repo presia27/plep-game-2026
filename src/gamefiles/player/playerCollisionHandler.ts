40dba230ae2e5ee72f3591191390087e54a74805
presia27 <pre.sia977@outlook.com>
2026-02-08 22:59:32 -0800

    Make it so only certain objects are treated as obstacles

--- src/gamefiles/player/playerCollisionHandler.ts
+++ src/gamefiles/player/playerCollisionHandler.ts
@@ -3,6 +3,7 @@ import { BoundingBox } from "../../componentLibrary/boundingBox.ts";
 import { AbstractCollisionHandler } from "../../componentLibrary/AbstractCollisionHandler.ts";
 import { MovementComponent } from "../../componentLibrary/movementComponent.ts";
 import { ISize } from "../../classinterfaces.ts";
+import { ShelfController } from "../shelves/shelfController.ts";
 
 /**
  * Player collision handler that prevents the player from
@@ -37,42 +38,45 @@ export class PlayerCollisionHandler extends AbstractCollisionHandler {
     const playerTop = this.boundingBox.getTop();
     const playerBottom = this.boundingBox.getBottom();
 
-    const shelfLeft = otherBounds.getLeft();
-    const shelfRight = otherBounds.getRight();
-    const shelfTop = otherBounds.getTop();
-    const shelfBottom = otherBounds.getBottom();
+    // Do not allow walking through the following objects
+    if (other instanceof ShelfController) {
+      const shelfLeft = otherBounds.getLeft();
+      const shelfRight = otherBounds.getRight();
+      const shelfTop = otherBounds.getTop();
+      const shelfBottom = otherBounds.getBottom();
 
-    // Calculate overlap on each axis
-    const overlapLeft = playerRight - shelfLeft;
-    const overlapRight = shelfRight - playerLeft;
-    const overlapTop = playerBottom - shelfTop;
-    const overlapBottom = shelfBottom - playerTop;
+      // Calculate overlap on each axis
+      const overlapLeft = playerRight - shelfLeft;
+      const overlapRight = shelfRight - playerLeft;
+      const overlapTop = playerBottom - shelfTop;
+      const overlapBottom = shelfBottom - playerTop;
 
-    // Find the smallest overlap to determine which side to push out from
-    const minOverlapX = Math.min(overlapLeft, overlapRight);
-    const minOverlapY = Math.min(overlapTop, overlapBottom);
+      // Find the smallest overlap to determine which side to push out from
+      const minOverlapX = Math.min(overlapLeft, overlapRight);
+      const minOverlapY = Math.min(overlapTop, overlapBottom);
 
-    // Push the player out on the axis with smallest penetration
-    if (minOverlapX < minOverlapY) {
-      // Resolve on X axis
-      if (overlapLeft < overlapRight) {
-        // Push player to the left
-        pos.x = shelfLeft - (xOffset + bbWidth);
+      // Push the player out on the axis with smallest penetration
+      if (minOverlapX < minOverlapY) {
+        // Resolve on X axis
+        if (overlapLeft < overlapRight) {
+          // Push player to the left
+          pos.x = shelfLeft - (xOffset + bbWidth);
+        } else {
+          // Push player to the right
+          pos.x = shelfRight - xOffset;
+        }
       } else {
-        // Push player to the right
-        pos.x = shelfRight - xOffset;
+        // Resolve on Y axis
+        if (overlapTop < overlapBottom) {
+          // Push player up
+          pos.y = shelfTop - (yOffset + bbHeight);
+        } else {
+          // Push player down
+          pos.y = shelfBottom - yOffset;
+        }
       }
-    } else {
-      // Resolve on Y axis
-      if (overlapTop < overlapBottom) {
-        // Push player up
-        pos.y = shelfTop - (yOffset + bbHeight);
-      } else {
-        // Push player down
-        pos.y = shelfBottom - yOffset;
-      }
+
+      this.movementComponent.setPosition(pos);
     }
-
-    this.movementComponent.setPosition(pos);
   }
 }
