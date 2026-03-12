452284ed81148fb68279b452274fdba69c6f31b2
presia27 <pre.sia977@outlook.com>
2026-03-11 18:58:35 -0700

    Remove individual files, change to minified bundled version

--- publishweb/js/gamefiles/deliveryEntity/deliveryCollisionHandler.js
+++
@@ -1,8 +0,0 @@
-import { AbstractCollisionHandler } from "../../componentLibrary/AbstractCollisionHandler.js";
-export class DeliveryCollisionHandler extends AbstractCollisionHandler {
-    constructor() {
-        super();
-    }
-    handleCollision(oth, boundingBox) {
-    }
-}
