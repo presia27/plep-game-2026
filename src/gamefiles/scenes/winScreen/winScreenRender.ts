1bc18826e697b92b8114c06b75528d94457514f4
presia27 <pre.sia977@outlook.com>
2026-03-15 00:02:45 -0700

    Add fade-in of background image

--- src/gamefiles/scenes/winScreen/winScreenRender.ts
+++ src/gamefiles/scenes/winScreen/winScreenRender.ts
@@ -1,9 +1,9 @@
 import { GameContext, IRenderer } from "../../../classinterfaces.ts";
 import { MultiLineTypewriter } from "../../../componentLibrary/multiLineTypewriter.ts";
 import { Entity } from "../../../entity.ts";
+import { ASSET_MANAGER } from "../../main.ts";
 
 export class WinScreenRender extends Entity {
-  private typewriter: MultiLineTypewriter;
 
   constructor() {
     super();
@@ -14,16 +14,46 @@ export class WinScreenRender extends Entity {
     ];
 
     const typewriter = new MultiLineTypewriter(texts, 30);
-    this.typewriter = typewriter;
     super.addComponent(typewriter);
+
+    const renderer = new Renderer(typewriter);
+    this.setRenderer(renderer);
   }
 
-  public override draw(context: GameContext): void {
+}
+
+class Renderer implements IRenderer {
+  private typewriter: MultiLineTypewriter;
+  private bgImage: HTMLImageElement;
+  
+  private fadeOpacity: number = 1;
+  private fadeSpeed: number = 0.5;
+
+  constructor(typewriter: MultiLineTypewriter) {
+    this.typewriter = typewriter;
+
+    const image = ASSET_MANAGER.getImageAsset("titleScreen");
+    if (image === null) {
+      throw new Error("Failed to load asset for the background image");
+    }
+    this.bgImage = image;
+  }
+
+  draw(context: GameContext): void {
     const ctx = context.ctx;
 
     ctx.save();
 
-    ctx.fillStyle = "black";
+    ctx.drawImage(this.bgImage, 0, 0, context.ctx.canvas.width, context.ctx.canvas.height);
+
+    if (this.fadeOpacity > 0) {
+      this.fadeOpacity = Math.max(
+        this.fadeOpacity - (this.fadeSpeed * context.clockTick),
+        0
+      );
+    }
+
+    ctx.fillStyle = `rgba(0, 0, 0, ${Math.max(this.fadeOpacity, 0.2)})`;
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
     ctx.fillStyle = "white";
